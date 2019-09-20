5f1d7e02e02bb4abbeb7a058041688040294edfb
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:47:42 +0200

    Negyedik feladat.

--- gyakorlas/Program.cs
+++ gyakorlas/Program.cs
@@ -10,39 +10,48 @@ namespace gyakorlas
     {
         static void Main(string[] args)
         {
-            List<int> lista = new List<int>();
-            int bekertSzam=0;
-            int max=0;
-            int min = 10000;
-            int bekeresSzama = 0;
-            Console.WriteLine("Kérem adja meg hány szám közül szeretné megkeresni a legnagybbat: ");
-            bekeresSzama = Convert.ToInt16(Console.ReadLine());
+            try
+            {
+                List<int> lista = new List<int>();
+                int bekertSzam = 0;
+                int max = 0;
+                int min = 10000;
+                int bekeresSzama = 0;
+                Console.WriteLine("Kérem adja meg hány szám közül szeretné megkeresni a legnagybbat: ");
+                bekeresSzama = Convert.ToInt16(Console.ReadLine());
 
-            for (int i = 0; i <bekeresSzama; i++)
-            {
-                Console.WriteLine("Kérem, adjon meg egy számot.");
-                bekertSzam = Convert.ToInt16(Console.ReadLine());
-                lista.Add(bekertSzam);
-              
-            }
-            for (int i = 0; i < lista.Count; i++)
-            {
-                if (max<lista[i])
+                for (int i = 0; i < bekeresSzama; i++)
                 {
-                   max=lista[i];
-                }
-                
-            }
-            Console.WriteLine("A legnagyobb szám:{0} ",max);
-            for (int i = 0; i < lista.Count; i++)
-            {
-                if (min > lista[i])
-                {
-                    min = lista[i];
-                }
+                    Console.WriteLine("Kérem, adjon meg egy számot.");
+                    bekertSzam = Convert.ToInt16(Console.ReadLine());
+                    lista.Add(bekertSzam);
 
+                }
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (max < lista[i])
+                    {
+                        max = lista[i];
+                    }
+
+                }
+                Console.WriteLine("A legnagyobb szám:{0} ", max);
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (min > lista[i])
+                    {
+                        min = lista[i];
+                    }
+
+                }
+                Console.WriteLine("A legkisebb szám: {0}", min);
             }
-            Console.WriteLine("A legkisebb szám: {0}",min);
+            catch (Exception)
+            {
+                Console.WriteLine("Kérem csak számot használjon."
+                    );
+            }
+            
             Console.ReadKey();
         }
         
